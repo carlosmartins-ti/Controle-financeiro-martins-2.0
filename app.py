f3658ea006f4d1f31946061a3bedbe827a1dc02b
carlos martins <cr954779@gmail.com>
2026-07-21 12:57:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-ar localmente: python app.py
-"""
+"""Executar localmente: python app.py"""
 import uvicorn
+
 
 if __name__ == "__main__":
     uvicorn.run("api.index:app", host="0.0.0.0", port=8000, reload=True)
